d5b63f8c927ff4fa7df5f37c1139fe7cbf8870ea
alha7503 <alexhallmann98@gmail.com>
2023-06-06 20:53:56 +0200

    Daten in Klassen Person und Test verpackt

--- person.py
+++ person.py
@@ -1,3 +1,36 @@
 class Person:
-    def __init__(self, age):
-        self.age = age
+    def __init__(self, id, gruppe, alter, uhrzeit, datum, vorerfahrung, stimmung, aufnahmefähigkeit, stresslevel, place, noise, test):
+        self.id = id
+        self.gruppe = gruppe
+        self.alter = alter
+        self.uhrzeit = uhrzeit
+        self.datum = datum
+        self.vorerfahrung = vorerfahrung
+        self.stimmung = stimmung
+        self.aufnahmefähigkeit = aufnahmefähigkeit
+        self.stresslevel = stresslevel
+        self.place = place
+        self.noise = noise
+        self.original = None
+        self.prototyp = None
+        if (test.type == "O"): self.original = test
+        else: self.prototyp = test
+
+    def addTest(self, test):
+        if(test.type == "O"): self.original = test
+        elif(test.type == "P"): self.prototyp = test
+    
+    def toString(self):
+        return self.id, self.gruppe, self.vorerfahrung, self.vorerfahrung, self.place
+        
+
+class Test:
+    def __init__(self, type, t_values, t_clicks, q_scores):
+        self.type = type
+        self.t_values = t_values
+        self.t_clicks = t_clicks
+        self.q_scores = q_scores
+
+    def toString(self):
+        return "t-values", self.t_values, "t_clicks", self.t_clicks, "q_scores", self.q_scores
+  
--- prepare_data.py
+++ prepare_data.py
@@ -1,13 +1,31 @@
 import pandas as pd
-from person import Person
+from person import Person, Test
 
-p1 = Person(34)
-print(p1)
+
+def doesExist(persons, id):
+    for person in persons:
+        if (person.id == id): return person
+    return False
+
 
 df = pd.read_csv("data.csv")
 df = df.drop(columns = ["Unnamed: 21", "Unnamed: 12", "Unnamed: 25"])
 
+persons = []
+for index, row in df.iterrows():
+    ## Testergebnis erstellen
+    t_values = [row["T1"], row["T2"], row["T3"], row["T4"]]
+    t_clicks = [row["T1Klicks"], row["T2Klicks"], row["T3Klicks"], row["T4Klicks"]]
+    q_scores = [row["Q1"], row["Q2"], row["Q3"]]
+    test = Test(row["System(P/O)"], t_values, t_clicks, q_scores)
+    
+    #schauen, ob Person mit ID existiert
+    if(doesExist(persons, row["ID (auf Zettel)"])):
+        person = doesExist(persons, row["ID (auf Zettel)"])
+        person.addTest(test)
+    else:
+        person = Person(row["ID (auf Zettel)"], row["Gruppe(PO/OP)"], row["Alter"], row["Uhrzeit"], row["Datum"], row["Vorerfahrung"], row["Stimmung"], row["Aufnahmefähigkeit"], row["Stresslevel"], row["inside/outside(i/o)"], row["Umgebungsgeräusche"], test)
+        persons.append(person)
 
+[print(person.toString()) for person in persons]
 
-for column in df:
-    print("'" + column + "'")
